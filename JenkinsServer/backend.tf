85ba0772f7b4e1be772eba5dfa1c0797564b4507
pandiansun77 <pandiansun77@gmail.com>
2026-02-19 20:58:18 -0500

    added new terraform code

--- JenkinsServer/backend.tf
+++ JenkinsServer/backend.tf
@@ -4,5 +4,12 @@ terraform {
     key    = "jenkins/terraform.tfstate"
     region = "us-east-1"
   }
+terraform {
+  backend "s3" {
+    bucket = "cicd-terraform-eks-pandian"
+    key    = "jenkins/terraform.tfstate"
+    region = "us-east-2"
+  }
+
 }
 
